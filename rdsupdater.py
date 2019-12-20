d1fa523d368925bf999c0161c06210b67b3bc1ad
Brady Bilderback <brady@bilderback.me>
2019-12-20 17:36:44 -0500

    updating error handling with urls

--- rdsupdater.py
+++ rdsupdater.py
@@ -22,18 +22,20 @@ class RDSUpdater:
             request = urllib.request.Request(url, headers={'User-Agent': 'RDS-Updater'})
             data = urllib.request.urlopen(request).read()
             data = json.loads(data.decode('utf-8'))
-
-            # set current song
-            c_title     = data[0]['song']
-            c_artist    = data[0]['artist']
-            c_album     = data[0]['album']
-
-            return { 'title': c_title, 'artist': c_artist, 'album': c_album }
             
         except (Exception, urllib.error.HTTPError) as e:
             print("HTTP Error => ", e)
+            data = [{'song': '91.9FM', 'artist': 'WMTU', 'album': 'WMTU'}]
         except (Exception, urllib.error.URLError) as e:
             print("URL Error => ", e)
+            data = [{'song': '91.9FM', 'artist': 'WMTU', 'album': 'WMTU'}]
+
+        # set current song
+        c_title     = data[0]['song']
+        c_artist    = data[0]['artist']
+        c_album     = data[0]['album']
+
+        return { 'title': c_title, 'artist': c_artist, 'album': c_album }
 
     # function to push an update to the RDS units
     def _update(self, title, artist):
